16c2b1ae61013c33f45e1bec417927829698b445
minuum <minwool0357@gmail.com>
2025-08-08 09:30:31 +0900

    feat: HDF5 데이터 추출 기능 추가 및 카메라 서비스 개선
    - extract_and_verify_h5.py 파일에 HDF5 데이터를 CSV 및 JSON 파일로 추출하는 기능 추가: export_to_csv 및 export_to_json 함수 구현.
    - 카메라 서비스 서버에서 카메라 재시작 기능 추가: reset_camera_service 서비스 구현.
    - 카메라 버퍼 플러시 및 최신 프레임 가져오기 기능 개선.
    - MobileVLADataCollector 클래스에서 시나리오 선택 및 진행 상황 표시 기능 추가.
    - .gitignore 파일에 *.h5 패턴 추가하여 HDF5 파일 무시.

---
+++ install/camera_interfaces/include/camera_interfaces/camera_interfaces/srv/get_image.hpp
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef CAMERA_INTERFACES__SRV__GET_IMAGE_HPP_
+#define CAMERA_INTERFACES__SRV__GET_IMAGE_HPP_
+
+#include "camera_interfaces/srv/detail/get_image__struct.hpp"
+#include "camera_interfaces/srv/detail/get_image__builder.hpp"
+#include "camera_interfaces/srv/detail/get_image__traits.hpp"
+#include "camera_interfaces/srv/detail/get_image__type_support.hpp"
+
+#endif  // CAMERA_INTERFACES__SRV__GET_IMAGE_HPP_
